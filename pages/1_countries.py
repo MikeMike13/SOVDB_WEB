4be6b3396e09dc20ada002a334eb53b8605e021f
Mike <nicolaev.m@gmail.com>
2024-04-08 00:32:37 +0300

    Timeline

--- pages/1_countries.py
+++ pages/1_countries.py
@@ -7,6 +7,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 from datetime import date, datetime
 import io
+from streamlit_timeline import st_timeline
+
 
 conn = ps.connect(database = "sovdb", 
                         user = "mike", 
@@ -37,6 +39,17 @@ def sovdb_read_date(ticker, date):
         return 0
     else:
         return rows[0][1]
+
+def sovdb_read_item(ticker, field, value):
+    query = "SELECT * FROM sovdb_schema.\""+ticker+"\" WHERE \""+field+"\"='"+str(value)+"'"    
+    cur = conn.cursor()
+    cur.execute(query);
+    rows = cur.fetchall()
+    rows = np.array([*rows])   
+    if rows.size ==0:
+        return 0
+    else:
+        return rows[0]
 
 def ticker_exists(ticker):
      query_s = "SELECT * FROM sovdb_schema.""macro_indicators"" WHERE ""ticker"" = '"+ticker+"'"    
@@ -780,7 +793,7 @@ if vs_peers:
                     x = np.random.normal(1, 0.04, size=1)
                     ax.plot(x, CPI_p[i], marker='.', color=mymap[0], alpha=0.4)
                     
-            plt.suptitle(countr+" ("+Big3_cat+") "+" vs "+vs_peers_name+" peers (macro)")
+            plt.suptitle(countr+" ("+Big3_cat+") vs "+vs_peers_name+" peers (macro)")
             plt.show() 
             st.pyplot(fig)
         with cols[1]:
@@ -813,7 +826,7 @@ if vs_peers:
                 else:
                     x = np.random.normal(1, 0.04, size=1)
                     ax.plot(x, EXTD_GDP_p[i], marker='.', color=mymap[0], alpha=0.4)    
-            plt.suptitle(countr+" ("+Big3_cat+") "+" vs "+vs_peers_name+" peers (external)")
+            plt.suptitle(countr+" ("+Big3_cat+") vs "+vs_peers_name+" peers (external)")
             plt.show() 
             st.pyplot(fig)
                     
@@ -858,7 +871,7 @@ if vs_peers:
                     ax.plot(x, GGE_GDP[i], marker='.', color=mymap[0], alpha=0.4)
                     
                     
-            plt.suptitle(countr+" ("+Big3_cat+") "+" vs "+vs_peers_name+" peers (fiscal 1)")
+            plt.suptitle(countr+" ("+Big3_cat+") vs "+vs_peers_name+" peers (fiscal 1)")
             plt.show() 
             st.pyplot(fig)
         
@@ -902,7 +915,7 @@ if vs_peers:
                     x = np.random.normal(1, 0.04, size=1)
                     ax.plot(x, GGINT_REV[i], marker='.', color=mymap[0], alpha=0.4)
                     
-            plt.suptitle(countr+" ("+Big3_cat+") "+" vs "+vs_peers_name+" peers (fiscal 2)")
+            plt.suptitle(countr+" ("+Big3_cat+") vs "+vs_peers_name+" peers (fiscal 2)")
             plt.show() 
             st.pyplot(fig)
         st.write(vs_peers_name + " peers ("+str(len(peers_cat_s)-1)+"): "+', '.join(peers_cat_s))            
@@ -1421,3 +1434,80 @@ download = st.download_button(
     file_name=countr+"_macro.xlsx",
     mime='application/vnd.ms-excel'
 )
+
+
+#st.set_page_config(layout="wide")
+st.subheader("EVENTS")
+table_name = key+"_EVENTS"       
+create_events_table = "CREATE TABLE IF NOT EXISTS sovdb_schema.\""+table_name+"\" ( ID SERIAL PRIMARY KEY, \"""Date\""" date, \"""tag\""" text, \"""des\""" text, \"""long_des\""" text)"    
+cur = conn.cursor()
+cur.execute(create_events_table)
+conn.commit()
+
+events = sovdb_read_gen(table_name)
+
+tags_events_all = []
+for event in events.tag:
+    x = event.split(',')
+    x = [a.strip() for a in x]
+    for x0 in x:
+        tags_events_all.append(x0)
+
+tags_events = st.selectbox("choose tag: ",pd.Series(tags_events_all).unique(), index=0)
+
+tag_ind = []
+for i in range(events.shape[0]):
+    if(tags_events in events.loc[i].tag):
+        tag_ind.append(float(events.loc[i].id))
+events_f = events[(events.id.isin(tag_ind))].sort_values(by=['Date'], ascending=False).reset_index()
+
+items = []    
+for i in range(events_f.shape[0]):
+    items.append({"id":str(events_f.loc[i].id),"content": events_f.loc[i].des, "start": events_f.loc[i].Date.strftime('%Y-%b-%d')})   
+
+timeline = st_timeline(items, groups=[], options={}, height="300px")
+st.write(events_f)
+if (timeline!=None):
+    sel_id = timeline.get('id')
+    selected_event = sovdb_read_item(table_name, 'id', sel_id)
+    
+    st.write(selected_event[1])
+    st.write(selected_event[3])
+    st.write(selected_event[4])
+    
+
+st.subheader("Add event")
+cols=st.columns(2) 
+with cols[0]:
+    event_date = st.date_input("Event date: ", pd.to_datetime('2000-12-31'))
+with cols[1]:
+    tags = st.text_input('Tags (, separate)', '')
+
+des = st.text_input('event, title', '')   
+long_des = st.text_input('event, des', '')   
+
+def click_button_add_event(Add_date,Add_tags,Add_event,Add_long_event):    
+    ticker = table_name
+    query = "INSERT INTO sovdb_schema.\""+ticker+"\" (\"""Date\""", \"""tag\""", \"""des\""", \"""long_des\""") VALUES ('"+Add_date.strftime('%d-%b-%Y')+"'::date, '"+Add_tags+"':: text, '"+Add_event+"'::text, '"+Add_long_event+"'::text)"
+    cur.execute(query)
+    conn.commit()
+    st.warning("New event was added")  
+    st.session_state.clicked = True
+
+def click_button_delete_event(DEL,event_id_del):    
+    ticker = table_name
+    query = "DELETE FROM sovdb_schema.\""+ticker+"\" WHERE \"""id\"""='"+str(event_id_del)+"'"
+    cur.execute(query)
+    conn.commit()
+    st.warning("Event was deleted")  
+    st.session_state.clicked = True
+    
+cols=st.columns(3)    
+with cols[0]: 
+    st.button('Add event', on_click=click_button_add_event, args=(event_date,tags,des,long_des)) 
+with cols[1]:
+    event_id_del = st.selectbox("id to delete: ",events_f.id, index=0)
+with cols[2]:
+    st.button('Delete event', on_click=click_button_delete_event, args=(1,event_id_del)) 
+    
+
